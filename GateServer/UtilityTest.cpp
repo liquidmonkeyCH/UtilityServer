7caf7af5283372e9cdd1e992997c040c373f6a59
hourui <47287984+liquidmonkeyCH@users.noreply.github.com>
2021-04-29 13:52:15 +0800

    1

--- GateServer/UtilityTest.cpp
+++ GateServer/UtilityTest.cpp
@@ -87,17 +87,47 @@ void com_scheduler(void)
 	com::scheduler<std::chrono::system_clock> m_scheduler;
 
 	m_scheduler.init(10);
-	m_scheduler.attach(std::chrono::system_clock::now() - std::chrono::microseconds(2000), []() {Clog::debug("haha0"); });
-	m_scheduler.attach(std::chrono::system_clock::now() + std::chrono::microseconds(4000), []() {Clog::debug("hoho"); });
-	m_scheduler.attach(std::chrono::system_clock::now() + std::chrono::microseconds(2000), []() {Clog::debug("haha1"); });
-	m_scheduler.attach(std::chrono::system_clock::now() + std::chrono::microseconds(2100), []() {Clog::debug("haha2"); });
-	m_scheduler.attach(std::chrono::system_clock::now() + std::chrono::microseconds(2500), []() {Clog::debug("haha3"); });
-	auto res = m_scheduler.attach(std::chrono::system_clock::now() + std::chrono::microseconds(2700), []() {Clog::debug("haha4"); });
+	m_scheduler.attach(std::chrono::microseconds(-2000), []() {Clog::debug("haha0"); });
+	m_scheduler.attach(std::chrono::microseconds(4000), []() {Clog::debug("hoho"); });
+	m_scheduler.attach(std::chrono::microseconds(2000), []() {Clog::debug("haha1"); });
+	m_scheduler.attach(std::chrono::microseconds(2100), []() {Clog::debug("haha2"); });
+	m_scheduler.attach(std::chrono::microseconds(2500), []() {Clog::debug("haha3"); });
+	auto res = m_scheduler.attach(std::chrono::microseconds(2700), []() {Clog::debug("haha4"); });
 	std::this_thread::sleep_for(std::chrono::microseconds(1400));
 	if (res.cancel()) Clog::debug("Cancel,%lld", std::chrono::system_clock::now().time_since_epoch().count());
 	m_scheduler.stop();
-	res = m_scheduler.attach(std::chrono::system_clock::now() + std::chrono::microseconds(2700), []() {Clog::debug("haha5"); });
+	res = m_scheduler.attach(std::chrono::microseconds(2700), []() {Clog::debug("haha5"); });
 	if (!res) Clog::debug("Bad event");
+}
+
+#include "Utility/task_dispatcher.hpp"
+#include "Utility/task_controler.hpp"
+#include "Utility/task_object.hpp"
+void task_object(void) {
+	task::dispatcher m_dispatcher;
+	task::controler m_controler;
+	m_dispatcher.start(10);
+	m_controler.init(&m_dispatcher);
+
+	task::object obj1;
+	task::object_channel channel1;
+	task::object_channel channel2;
+
+	obj1.init(&m_controler);
+	channel1.init(&m_controler);
+	channel2.init(&m_controler);
+	
+	obj1.exec([]() {std::this_thread::sleep_for(std::chrono::microseconds(100)); std::cout << "obj1.exec 1" << std::endl; });
+	obj1.enter_channel(channel1.to_channel());
+	channel1.exec([]() {std::this_thread::sleep_for(std::chrono::microseconds(100)); std::cout << "channel1.exec 1" << std::endl; });
+	channel1.enter_channel(channel2.to_channel());
+	channel2.exec([]() {std::this_thread::sleep_for(std::chrono::microseconds(100)); std::cout << "channel2.exec 1" << std::endl; });
+	obj1.exec([&]() { std::cout << "obj1.exec 2"  << std::endl; });
+	channel1.exec([&]() { std::cout << "channel1.exec 2" << std::endl; });
+	channel2.exec([]() { std::cout << "channel2.exec 2" << std::endl; });
+	channel1.close();
+	std::this_thread::sleep_for(std::chrono::seconds(10));
+	m_dispatcher.stop();
 }
 
 void UtilityTest::run(void)
@@ -106,4 +136,5 @@ void UtilityTest::run(void)
 	com_hex_caster();
 	com_md5();
 	com_scheduler();
+	task_object();
 }
